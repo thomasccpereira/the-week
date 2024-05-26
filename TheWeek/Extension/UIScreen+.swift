a18389dae456209082022b0a0d966c7f1c9890d8
Thomás Pereira <thomasccpereira@Thomass-MacBook-Air.local>
2024-05-26 14:45:40 +0100

    feature: created extension to get screen width

---
+++ TheWeek/Extension/UIScreen+.swift
@@ -0,0 +1,7 @@
+import UIKit
+
+extension UIScreen {
+   static var size: CGSize { UIScreen.main.bounds.size }
+   static var width: CGFloat { size.width }
+   static var height: CGFloat { size.height }
+}
